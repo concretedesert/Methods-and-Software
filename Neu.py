43a2d5622b9e420bb9a8f34ae45a0e7d0c993d96
concretedesert <m.k.1@web.de>
2020-12-03 13:09:54 +0100

    Update Neu.py

--- Neu.py
+++ Neu.py
@@ -5,11 +5,11 @@ Created on Fri Nov 27 20:54:16 2020
 @author: mk1
 """
 
-import numpy as np
-
 class Request:
     alpha = 10 # minutes
     service_time = 2 # minutes
+    visit_origin = -1 # not planned yet
+    visit_dest = -1 # not planned yet
     
     def euclidean_distance(self):
         return ((self.start_loc[0] - self.dest_loc[0])**2 
@@ -19,8 +19,24 @@ class Request:
         self.start_loc = start_loc
         self.dest_loc = dest_loc
         self.req_time = req_time
-        
         self.end_time = self.req_time + self.euclidean_distance() + self.alpha
+        
+    def check_visit_times(self, t_origin, t_dest):
+        if t_origin < self.req_time or t_origin > t_dest or t_dest > self.end_time:
+            return False
+        return True
+    
+    def check_visit_start(self, t_origin):
+        return self.req_time <= t_origin <= self.end_time
+    
+    def check_visit_dest(self, t_origin, t_dest):
+        return self.req_time <= t_dest <= self.end_time and t_dest > t_origin 
+    
+    def set_visit_times(self, t_origin, t_dest):
+        if self.check_visit_times(t_origin, t_dest):
+            raise("Fehlerhafte Besuchszeiten")
+        self.visit_origin = t_origin
+        self.visit_dest = t_dest
         
 class Tourplan:
     
@@ -47,11 +63,34 @@ class Tourplan:
             - self.euclidean_distance(self.coordinates[i-1], self.coordinates[i]) \
             - self.euclidean_distance(self.coordinates[j-1], self.coordinates[j])
     
+    def insert_costs_single(self, loc, i): # startordest = 0 / 1
+        return self.euclidean_distance(self.coordinates[i-1], loc) \
+        + self.euclidean_distance(loc, self.coordinates[i]) \
+        - self.euclidean_distance(self.coordinates[i-1], self.coordinates[i]) 
+    
     def insert_feasible(self, request, i, j):
-        return True # Zeitfenster implementieren
+        t_start = self.euclidean_distance(self.coordinates[i-1], request.start_loc) \
+            + self.euclidean_distance(request.start_loc, self.coordinates[i])
+        lag_start = self.insert_costs_single(request.start_loc, i)
+        t_dest = self.euclidean_distance(self.coordinates[j-1], request.dest_loc) \
+            + self.euclidean_distance(request.dest_loc, self.coordinates[j]) \
+            + lag_start
+        lag_dest = self.insert_costs_single(request.dest_loc, j) \
+            + lag_start
+        
+        if not request.check_visit_times(t_start, t_start):
+            return False
+        
+        for place in self.tourenplan[i:j]:
+            if place[1] == 0:
+                
+        
+    
     
     def insert(self, request, i, j):
-        pass
+        self.tourenplan()
+
+# Wie entscheiden, welcher Kunde zuerst eingefügt wird
 
 def parallel_insertion(tourplan, requests):
     while requests:
